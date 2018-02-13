351b394a7a6da5eb65ee1060498c7d6ef062b893
Mia Wölm <mia.wl095@googlemail.com>
2018-02-13 18:16:32 +0100

    Buttons zum Wechseln der Etagen

--- frontend/src/Lageplan/Altbau.js
+++ frontend/src/Lageplan/Altbau.js
@@ -40,7 +40,9 @@ class Altbau extends Component {
             <div className="lageplan-wrapper">
                 <div className="sidebar">
                   <input className="search" placeholder="Suche nach einem Raum..." value={this.state.search} onChange={(e) => this.handleChange(e)} />
-                  <button onClick={() => this.changeFloor(0)}>Basement Floor</button>
+                  <button className={`change-floor ${this.state.floor === 0 && 'active'}`} value="0" onClick={() => this.changeFloor(0)}>UG</button>
+                  <button className={`change-floor ${this.state.floor === 1 && 'active'}`} value="1" onClick={() => this.changeFloor(1)}>EG</button>
+                  <button className={`change-floor ${this.state.floor === 2 && 'active'}`} value="2" onClick={() => this.changeFloor(2)}>1</button>
                 </div>
                 <div className="lageplan">
                   {this.floorPlan(this.state.floor, this.state.search)}
