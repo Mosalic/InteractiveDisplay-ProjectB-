68304ef2ef776698fabc872ad97ad15e26ed4df9
Mia Wölm <mia.wl095@googlemail.com>
2018-03-14 18:28:47 +0100

    Fixes das die ersten Buttons nicht überall angezeigt werden

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Home from './Home';
+import Main from './Main';
 import './App.css';
 import Admin from './Admin';
 
@@ -9,7 +9,7 @@ class App extends Component {
     return (
         <Router>
           <div>
-            <Route path="/home" component={Home}/>
+            <Route path="/home" component={Main}/>
             <Route path="/admin" component={Admin}/>
           </div>
         </Router>
--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Link } from 'react-router-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Lageplan from './Lageplan/index';
 import AllgemeineInformationen from './AllgemeineInformationen';
 import Header from './Header/Header';
@@ -14,39 +13,27 @@ class Home extends Component {
   render() {
     console.log(this.props);
     return (
-    <div className="App">
-      <Header />
-      <div className="main">
-        <div className="wrapper">
+      <div className="wrapper">
 
-          <Route exact path={`${this.props.match.url}/lageplan`} component={Lageplan}/>
-          <Route path="/home/allgemeineInformationen" component={AllgemeineInformationen}/>
-          <Route exact path="/home/altbau" component={Altbau}/>
-          <Route exact path="/home/spiele" component={Spiele}/>
-          <Route exact path="/home/professorenListe" component={ProfessorenListe}/>
-
-
-            <Link to="/home/allgemeineInformationen">
-                <div className="button">
-                    Allgemeine Informationen
-                </div>
-            </Link>
-            <Link to="/home/lageplan">
-                <div className="button">
-                    Lageplan
-                </div>
-            </Link>
-                <div className="button">
-                    Schwarzes Brett
-                </div>
-            <Link to="/home/professorenListe">
-                <div className="button">
-                    Spiele/TestProfessoren
-                </div>
-            </Link>
-        </div>
+          <Link to="/home/allgemeineInformationen">
+              <div className="button">
+                  Allgemeine Informationen
+              </div>
+          </Link>
+          <Link to="/home/lageplan">
+              <div className="button">
+                  Lageplan
+              </div>
+          </Link>
+              <div className="button">
+                  Schwarzes Brett
+              </div>
+          <Link to="/home/professorenListe">
+              <div className="button">
+                  Spiele/TestProfessoren
+              </div>
+          </Link>
       </div>
-    </div>
     );
   }
 }
