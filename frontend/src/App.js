32cb3835c93f507daecb4ad903267d8129e6174a
Röttger <mona.roettger@gmail.com>
2018-03-16 15:55:34 +0100

    die Build des MemoryGames eingefügt, neue Komponente für ProfessorenListe  und UserInput (eintragen von Aushängen) erstellt

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './Home';
 import './App.css';
 import Admin from './Admin';
+import UserInput from './User/UserInput';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
           <div>
             <Route path="/home" component={Home}/>
             <Route path="/admin" component={Admin}/>
+            <Route path="/userInput" component={UserInput}/>
           </div>
         </Router>
     );
