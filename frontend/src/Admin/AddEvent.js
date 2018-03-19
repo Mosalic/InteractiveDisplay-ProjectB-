2c65b62f47dc4032932184d351d41d5e28917626
Mia Wölm <mia.wl095@googlemail.com>
2018-03-20 00:57:47 +0100

    AddEvent Window hinzugefügt

---
+++ frontend/src/Admin/AddEvent.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class AddEvent extends Component{
+  constructor(){
+    super();
+
+    this.state = {
+      name: '',
+      date: '',
+      time: '',
+      information: '',
+      place: '',
+      imgUrl: '',
+      base64: require('./dummy-image.jpeg'),
+    }
+  }
+
+  save(){
+    let formData = new FormData();
+    formData.append('img', this.state.imgUrl);
+    formData.append('name', this.state.name);
+    formData.append('date', this.state.date);
+    formData.append('time', this.state.time);
+    formData.append('information', this.state.information);
+    formData.append('place', this.state.place);
+    axios.post('http://localhost:3001/events', formData, {headers:{ Authorization: localStorage.getItem('JWTToken'), 'Content-Type': 'multipart/form-data'}})
+    .then((response) => {
+      console.log('Event added');
+    })
+    .catch((error) => {
+      console.log('error', error);
+    })
+  }
+
+  handleChange(e){
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  encodeImageFileAsURL(element){
+      this.setState({
+          imgUrl: element.target.files[0]
+      });
+      var file = element.target.files[0];
+      var reader = new FileReader();
+      reader.onloadend = function() {
+          this.setState({
+              base64: reader.result,
+          })
+      }.bind(this)
+      reader.readAsDataURL(file);
+  }
+
+  render(){
+    return(
+      <div className="backdrop">
+        <div className="modal">
+          <h2>Event hinzufügen</h2>
+          <button className="btn-close" type="button" onClick={() => this.props.close()}>x</button>
+          <form className="event">
+            <div className="row">Eventfoto <input id="foto" name="foto" type="file" onChange={(event) => this.encodeImageFileAsURL(event)} accept="image/x-png,image/gif,image/jpeg" />
+            <label htmlFor="foto"><div className="professor-foto" style={{backgroundImage: `url(${this.state.base64})`}}></div></label></div>
+            <div className="row">Eventname <input value={this.state.name} placeholder="Name" onChange={(e) => this.handleChange(e)} name="name" /></div>
+            <div className="row">Eventdatum <input value={this.state.date} placeholder="Date" type="date" onChange={(e) => this.handleChange(e)} name="date" /></div>
+            <div className="row">Eventzeit <input value={this.state.time} placeholder="Time" onChange={(e) => this.handleChange(e)} name="time" /></div>
+            {/* <div className="row">Eventbeschreibung <input value={this.state.information} placeholder="Information" onChange={(e) => this.handleChange(e)} name="information" /></div> */}
+            <div className="row">Eventbeschreibung  <textarea value={this.state.information} name="information" placeholder="Eventbeschreibung" onChange={(e) => this.handleChange(e)} /></div>
+            <div className="row">Eventort <input value={this.state.place} placeholder="Place" onChange={(e) => this.handleChange(e)} name="place" /></div>
+          </form>
+          <div className="button-footer">
+            <button className="btn-save" type="button" onClick={() => this.save()}>Save</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AddEvent;
--- frontend/src/Admin/Events.js
+++ frontend/src/Admin/Events.js
@@ -1,69 +1,77 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import FontAwesome from 'react-fontawesome';
+import AddEvent from './AddEvent';
 
 class Events extends Component {
   constructor(){
     super();
 
     this.state = {
-      name: '',
-      date: '',
-      time: '',
-      information: '',
-      place: '',
-      imgUrl: '',
-      base64: require('./dummy-image.jpeg'),
+      events: [],
+      monthMapping: ['JAN', 'FEB', 'MÄR', 'APR', 'MAI', 'JUN', 'JUL', 'AUG', 'SEP', 'OKT', 'NOV', 'DEZ'],
+      addEventVisible: false,
     };
   }
 
-  handleChange(e){
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
-  }
-
-  encodeImageFileAsURL(element){
-      this.setState({
-          imgUrl: element.target.files[0]
-      });
-      var file = element.target.files[0];
-      var reader = new FileReader();
-      reader.onloadend = function() {
-          this.setState({
-              base64: reader.result,
-          })
-      }.bind(this)
-      reader.readAsDataURL(file);
-  }
-
-  save(){
-    let formData = new FormData();
-    formData.append('img', this.state.imgUrl);
-    formData.append('name', this.state.name);
-    formData.append('date', this.state.date);
-    formData.append('time', this.state.time);
-    formData.append('information', this.state.information);
-    formData.append('place', this.state.place);
-    axios.post('http://localhost:3001/events', formData, {headers:{ Authorization: localStorage.getItem('JWTToken'), 'Content-Type': 'multipart/form-data'}})
+  componentDidMount(){
+    axios.get('http://localhost:3001/events')
     .then((response) => {
-      console.log('Event added');
+      this.setState({
+        events: response.data.events,
+      });
     })
     .catch((error) => {
       console.log('error', error);
     })
   }
 
+  toggleAddEvent(){
+    this.setState({
+      addEventVisible: !this.state.addEventVisible,
+    })
+  }
+
   render(){
     return(
       <div>
-        <input value={this.state.name} placeholder="Name" onChange={(e) => this.handleChange(e)} name="name" />
-        <input value={this.state.date} placeholder="Date" type="date" onChange={(e) => this.handleChange(e)} name="date" />
-        <input value={this.state.time} placeholder="Time" onChange={(e) => this.handleChange(e)} name="time" />
-        <input value={this.state.information} placeholder="Information" onChange={(e) => this.handleChange(e)} name="information" />
-        <input value={this.state.place} placeholder="Place" onChange={(e) => this.handleChange(e)} name="place" />
-        <input id="foto" name="foto" type="file" onChange={(event) => this.encodeImageFileAsURL(event)} accept="image/x-png,image/gif,image/jpeg" />
-        <label htmlFor="foto"><div className="professor-foto" style={{backgroundImage: `url(${this.state.base64})`}}></div></label>
-        <button onClick={() => this.save()}>Save</button>
+        <button type="button" className="add" onClick={() => this.toggleAddEvent()}>+</button>
+        <div className="event">
+          <div className="events">
+            {this.state.events.map((event, index) =>
+              <div className="event__box" key={index}>
+
+                <div className="event__photo" style={{backgroundImage: `${event.img ? `url(data:image/png;base64,${new Buffer(event.img.data, 'binary').toString('base64')})`: `url(${require('../User/pinboard-icon.png')})`}`}}>
+                </div>
+                <div className="event__information">
+                  <div className="event__heading">
+                    <div className="event__date">
+                      <div className="event__day">{new Date(event.date).getDate()}</div>
+                      <div className="event__month">{this.state.monthMapping[new Date(event.date).getMonth()]}</div>
+                    </div>
+                    <div className="event__title">
+                      {event.name}
+                    </div>
+                  </div>
+                  <div className="event__subheading">
+                    <div><FontAwesome name="map-marker-alt" /> {event.place}</div>
+                    <div><FontAwesome name="clock" /> {event.time}</div>
+                  </div>
+                  {/*Informationen aus der Datenbank werden zugewiesen*/}
+                  <div className="event__main">
+                    {event.information}
+                  </div>
+                </div>
+
+              </div>
+            )}
+          </div>
+        </div>
+        {this.state.addEventVisible &&
+          <AddEvent
+            close={() => this.toggleAddEvent()}
+          />
+        }
       </div>
     );
   }
